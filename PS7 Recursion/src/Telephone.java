adb1f0a9a851008ef6106cc54d24697ab4206bf7
coryupham <coryupham@gmail.com>
2021-10-12 16:07:51 -0700

    Question 5 was a bitch!

--- PS7 Recursion/src/Telephone.java
+++ PS7 Recursion/src/Telephone.java
@@ -11,25 +11,25 @@ public class Telephone {
 
     public Telephone(String phone) {
 
-        generate(phone, phone.length(), "", 0, 0);
+        generate(phone, phone.length(), "", 0);
     }
 
-    public void generate(String phone, int maxLength, String result, int counter, int letterCounter) {
+    public void generate(String phone, int maxLength, String result, int counter) {
         if (result.length() < maxLength) {
             char current = phone.charAt(counter);
             int digit = Integer.parseInt("" + current);
             String subset = mapping[digit];
             if(digit == 0 || digit == 1){
-                generate(phone, maxLength, result + subset.charAt(0), counter + 1, letterCounter);
+                generate(phone, maxLength, result + subset.charAt(0), counter + 1);
             } else if (digit == 7|| digit == 9) {
-                generate(phone, maxLength, result + subset.charAt(0), counter + 1, letterCounter);
-                generate(phone, maxLength, result + subset.charAt(1), counter + 1, letterCounter);
-                generate(phone, maxLength, result + subset.charAt(2), counter + 1, letterCounter);
-                generate(phone, maxLength, result + subset.charAt(3), counter + 1, letterCounter);
+                generate(phone, maxLength, result + subset.charAt(0), counter + 1);
+                generate(phone, maxLength, result + subset.charAt(1), counter + 1);
+                generate(phone, maxLength, result + subset.charAt(2), counter + 1);
+                generate(phone, maxLength, result + subset.charAt(3), counter + 1);
             } else {
-                generate(phone, maxLength, result + subset.charAt(0), counter + 1, letterCounter);
-                generate(phone, maxLength, result + subset.charAt(1), counter + 1, letterCounter);
-                generate(phone, maxLength, result + subset.charAt(2), counter + 1, letterCounter);
+                generate(phone, maxLength, result + subset.charAt(0), counter + 1);
+                generate(phone, maxLength, result + subset.charAt(1), counter + 1);
+                generate(phone, maxLength, result + subset.charAt(2), counter + 1);
             }
 
         } else {
